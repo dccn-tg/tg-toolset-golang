1e43da8696a4ea7b7f50fabd7bc919604da32f7d
Hurng-Chun Lee <hclee.work@gmail.com>
2019-04-06 11:44:50 +0200

    start implementing logic for volume creation

--- go.mod
+++ go.mod
@@ -8,7 +8,7 @@ require (
 	github.com/pkg/errors v0.8.1
 	github.com/sirupsen/logrus v1.4.0
 	github.com/spf13/viper v1.3.2
-	golang.org/x/crypto v0.0.0-20190308221718-c2843e01d9a2 // indirect
+	golang.org/x/crypto v0.0.0-20190308221718-c2843e01d9a2
 	golang.org/x/sys v0.0.0-20190222072716-a9d3bda3a223 // indirect
-	google.golang.org/appengine v1.4.0 // indirect
+	google.golang.org/appengine v1.4.0
 )
--- project/internal/vol/vol.go
+++ project/internal/vol/vol.go
@@ -1,5 +1,18 @@
 package vol
 
+import (
+	"bytes"
+	"io"
+	"io/ioutil"
+	"os/user"
+	"path"
+	"regexp"
+	"strconv"
+
+	log "github.com/sirupsen/logrus"
+	"golang.org/x/crypto/ssh"
+)
+
 // VolumeManager defines functions for managing storage volume for a project.
 type VolumeManager interface {
 	// Create provisions a project volume on the targeting storage system.
@@ -16,17 +29,105 @@ type NetAppVolumeManager struct {
 	AddressFilerMI string
 }
 
+// connect makes a SSH connection to the NetApp filer management interface and
+// returns a ssh.Session.
+func (m NetAppVolumeManager) connect() (session *ssh.Session, err error) {
+	// get current user's SSH privat key $HOME/.ssh/id_rsa
+	me, err := user.Current()
+	if err != nil {
+		return
+	}
+
+	key, err := ioutil.ReadFile(path.Join(me.HomeDir, ".ssh/id_rsa"))
+	if err != nil {
+		return
+	}
+
+	signer, err := ssh.ParsePrivateKey(key)
+	if err != nil {
+		return
+	}
+
+	config := &ssh.ClientConfig{
+		User: "admin",
+		Auth: []ssh.AuthMethod{
+			ssh.PublicKeys(signer),
+		},
+		HostKeyCallback: ssh.InsecureIgnoreHostKey(),
+	}
+
+	client, err := ssh.Dial("tcp", m.AddressFilerMI, config)
+	if err != nil {
+		return
+	}
+
+	session, err = client.NewSession()
+	if err != nil {
+		return
+	}
+
+	return
+}
+
 // Create provisions a project volume on the NetApp's ONTAP cluster filer.
 func (m NetAppVolumeManager) Create(projecID string, quotaGiB int64) error {
 	// TODO: create SSH command for the filer's management interface.
 
+	session, err := m.connect()
+	if err != nil {
+		return err
+	}
+	defer session.Close()
+
 	// filer manager command to list aggregates,
 	// storage aggregate show -fields availsize,volcount -stat online
-	//
+	var b bytes.Buffer
+	session.Stdout = &b
+	if err := session.Run("storage aggregate show -fields availsize,volcount -stat online"); err != nil {
+		return err
+	}
+
 	// regex to parse output: regexp.Compile("^(aggr\S+)\s+(\S+[P,T,G,M,K]B)\s+([0-9]+)$")
 	// - field 1: aggregation name
 	// - field 2: free space
 	// - field 3: number of volumes in the aggregate
+	type aggregateInfo struct {
+		name      string
+		freeSpace uint64
+		nvols     uint16
+	}
+
+	var aggregates []aggregateInfo
+	reAggrInfo := regexp.MustCompile(`^(aggr\S+)\s+(\S+[P,T,G,M,K]B)\s+([0-9]+)$`)
+	for {
+		line, err := b.ReadString('\n')
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			return err
+		}
+		if d := reAggrInfo.FindAllStringSubmatch(line, -1); d != nil {
+			log.Debugf("aggregate: %s\n", line)
+			var freeSpace, nvols int
+			if freeSpace, err = strconv.Atoi(d[0][2]); err != nil {
+				log.Debugf("cannot parse freespace of aggregate: %s\n", d[0][2])
+				continue
+			}
+			if nvols, err = strconv.Atoi(d[0][3]); err != nil {
+				log.Debugf("cannot parse nvols of aggregate: %s\n", d[0][3])
+				continue
+			}
+
+			// TODO: convert freeSpace to unit of bytes
+
+			aggregates = append(aggregates, aggregateInfo{
+				name:      d[0][1],
+				freeSpace: uint64(freeSpace),
+				nvols:     uint16(nvols),
+			})
+		}
+	}
 
 	// filer manager command to list QoS policies
 	// qos policy-group show
