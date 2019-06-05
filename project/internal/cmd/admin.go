98e88467669f51d011bfad6bf5cfab7432da619e
Hurng-Chun Lee <h.lee@donders.ru.nl>
2019-06-05 23:16:55 +0200

    conditionally hide admin command

--- project/internal/cmd/admin.go
+++ project/internal/cmd/admin.go
@@ -1,12 +1,41 @@
 package cmd
 
-import "github.com/spf13/cobra"
+import (
+	"os/user"
+
+	"github.com/spf13/cobra"
+)
 
 var configFile string
 
+// showAdminCmd defines a function to determine whether the admin command should be shown.
+var showAdminCmd = func() bool {
+	me, err := user.Current()
+
+	if err != nil {
+		return false
+	}
+
+	grp, err := user.LookupGroupId(me.Gid)
+	if err != nil {
+		return false
+	}
+
+	// allows "root" or "tg" member to see the admin subcommand
+	if me.Username == "root" || grp.Name == "tg" {
+		return true
+	}
+
+	return false
+}
+
 func init() {
 	adminCmd.PersistentFlags().StringVarP(&configFile, "config", "c", "config.xml", "path of the configuration YAML file.")
-	rootCmd.AddCommand(adminCmd)
+
+	// only show admin command if the user is root
+	if showAdminCmd() {
+		rootCmd.AddCommand(adminCmd)
+	}
 }
 
 var adminCmd = &cobra.Command{
