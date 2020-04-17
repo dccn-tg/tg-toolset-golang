8f9ac561e5083e2e85ee355fab68d8b8d24d1881
Hurng-Chun Lee <h.lee@donders.ru.nl>
2020-04-17 10:48:07 +0200

    implemented graphql queries

--- project/pkg/pdb2/pdb2.go
+++ project/pkg/pdb2/pdb2.go
@@ -17,6 +17,7 @@ import (
 	"golang.org/x/oauth2"
 
 	log "github.com/Donders-Institute/tg-toolset-golang/pkg/logger"
+	"github.com/Donders-Institute/tg-toolset-golang/project/pkg/acl"
 )
 
 var (
@@ -25,6 +26,18 @@ var (
 
 	// AUTH_SERVER_URL is the authentication server URL.
 	AUTH_SERVER_URL string
+
+	// action2role maps the pending role action string of the Core API
+	// to the string representation of the `acl.Role`, and can be used
+	// directly for the API of the filer-gateway:
+	//
+	// https://github.com/Donders-Institute/filer-gateway
+	action2role map[string]string = map[string]string{
+		"SetToManager":     acl.Manager.String(),
+		"SetToContributor": acl.Contributor.String(),
+		"SetToViewer":      acl.Viewer.String(),
+		"Unset":            "none",
+	}
 )
 
 func init() {
@@ -39,22 +52,81 @@ func init() {
 	log.NewLogger(cfg, log.InstanceLogrusLogger)
 }
 
-// ProjectPendingRole is the data structure of a pending role setting on a project.
-type ProjectPendingRole struct {
-	ProjectID string `json:"projectID"`
-	Username  string `json:"username"`
-	Role      string `json:"role"`
+// member is the data structure of a pending role setting on a project.
+type member struct {
+	UserID string `json:"userID"`
+	Role   string `json:"role"`
 }
 
-// GetProjectPendingRoles retrieves a list of project pending roles from the project
-// database.
-func GetProjectPendingRoles(authClientSecret string) ([]ProjectPendingRole, error) {
+// storage is the data structure for project quota information.
+type storage struct {
+	QuotaGb int    `json:"quotaGb"`
+	System  string `json:"system"`
+}
 
-	pendingRoles := make([]ProjectPendingRole, 0)
+// DataProjectProvision defines the data structure for project provisioning with
+// given storage quota and data-access roles.
+type DataProjectProvision struct {
+	ProjectID string   `json:"projectID"`
+	Members   []member `json:"members"`
+	Storage   storage  `json:"storage"`
+}
+
+// DataProjectUpdate defines the data structure for project update with given
+// storage quota and data-access roles.
+type DataProjectUpdate struct {
+	Members []member `json:"members"`
+	Storage storage  `json:"storage"`
+}
+
+// GetProjectPendingActions performs queries on project pending roles and project storage
+// resource, and combines the results into data structure that can be directly used for
+// updating project storage resources and member roles via the filer-gateway API:
+// https://github.com/Donders-Institute/filer-gateway
+func GetProjectPendingActions(authClientSecret string) (map[string]DataProjectUpdate, error) {
+	actions := make(map[string]DataProjectUpdate)
+
+	return actions, nil
+}
+
+// getProjectStorageResource retrieves the storage resource information of a given project.
+func getProjectStorageResource(authClientSecret, projectID string) (*storage, error) {
+	var stor storage
 
 	// GraphQL query construction
 	var qry struct {
-		PendingProjectMemberChanges struct {
+		Project struct {
+			QuotaGb graphql.Int
+		} `graphql:"project(number: $id)"`
+	}
+
+	vars := map[string]interface{}{
+		"id": graphql.ID(projectID),
+	}
+
+	if err := query(authClientSecret, &qry, vars); err != nil {
+		log.Errorf("fail to query project quota: %s", err)
+		return nil, err
+	}
+
+	// TODO: do not hardcode system to "netapp".
+	stor = storage{
+		QuotaGb: int(qry.Project.QuotaGb),
+		System:  "netapp",
+	}
+
+	return &stor, nil
+}
+
+// getProjectPendingRoles retrieves a list of project pending roles from the project
+// database.
+func getProjectPendingRoles(authClientSecret string) (map[string][]member, error) {
+
+	pendingRoles := make(map[string][]member)
+
+	// GraphQL query construction
+	var qry struct {
+		PendingProjectMemberChanges []struct {
 			Project struct {
 				Number graphql.String
 			}
@@ -65,19 +137,45 @@ func GetProjectPendingRoles(authClientSecret string) ([]ProjectPendingRole, erro
 		} `graphql:"pendingProjectMemberChanges"`
 	}
 
+	if err := query(authClientSecret, &qry, nil); err != nil {
+		log.Errorf("fail to query project pending roles: %s", err)
+		return pendingRoles, err
+	}
+
+	for _, rc := range qry.PendingProjectMemberChanges {
+
+		pid := string(rc.Project.Number)
+
+		if _, ok := pendingRoles[pid]; !ok {
+			pendingRoles[pid] = make([]member, 0)
+		}
+
+		pendingRoles[pid] = append(pendingRoles[pid], member{
+			UserID: string(rc.Member.Username),
+			Role:   action2role[string(rc.Action)],
+		})
+	}
+
+	return pendingRoles, nil
+}
+
+// query is a generic function for GraphQL query.  It constructs a http client with a valid
+// api token and performs graphql query to the project database's core api.
+func query(authClientSecret string, qry interface{}, vars map[string]interface{}) error {
 	// Perform query
 	client, err := newClient(authClientSecret)
 	if err != nil {
-		return pendingRoles, err
+		return err
 	}
-	err = client.Query(context.Background(), &qry, nil)
+	err = client.Query(context.Background(), qry, vars)
 	if err != nil {
-		return pendingRoles, err
+		return err
 	}
 
 	// Loop over qry and feed pendingRoles
+	log.Debugf("qry result: %+v", qry)
 
-	return pendingRoles, nil
+	return nil
 }
 
 // newClient returns a GraphQL client with proper authentication via the
@@ -96,7 +194,7 @@ func newClient(authClientSecret string) (*graphql.Client, error) {
 	)
 	httpClient := oauth2.NewClient(context.Background(), src)
 
-	return graphql.NewClient("https://example.com/graphql", httpClient), nil
+	return graphql.NewClient(PDB_CORE_API_URL, httpClient), nil
 }
 
 // getAuthToken retrieves the OAuth client token from the authentication server,
--- project/pkg/pdb2/pdb2_test.go
+++ project/pkg/pdb2/pdb2_test.go
@@ -29,3 +29,25 @@ func TestGetAuthToken(t *testing.T) {
 	}
 	t.Logf("token: %+v\n", token)
 }
+
+func TestGetProjectPendingRoles(t *testing.T) {
+	PDB_CORE_API_URL = os.Getenv("PDB_CORE_API_URL")
+	AUTH_SERVER_URL = os.Getenv("AUTH_SERVER_URL")
+
+	pendingRoles, err := getProjectPendingRoles(os.Getenv("PDB_CORE_API_CLIENT_SECRET"))
+	if err != nil {
+		t.Errorf("%s\n", err)
+	}
+	t.Logf("pendingRoles: %+v\n", pendingRoles)
+}
+
+func TestGetProjectStorageResource(t *testing.T) {
+	PDB_CORE_API_URL = os.Getenv("PDB_CORE_API_URL")
+	AUTH_SERVER_URL = os.Getenv("AUTH_SERVER_URL")
+
+	stor, err := getProjectStorageResource(os.Getenv("PDB_CORE_API_CLIENT_SECRET"), "3010000.03")
+	if err != nil {
+		t.Errorf("%s\n", err)
+	}
+	t.Logf("storage resource: %+v\n", stor)
+}
