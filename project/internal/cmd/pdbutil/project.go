c953c56ab6916f22f1ebcb72cc5a2b6e2b305872
Hurng-Chun Lee <h.lee@donders.ru.nl>
2020-06-25 10:49:32 +0200

    added switch between ontap cli and filer-gateway

--- project/internal/cmd/pdbutil/project.go
+++ project/internal/cmd/pdbutil/project.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"os"
+	"os/user"
 	"path/filepath"
 	"strconv"
 	"strings"
@@ -18,7 +19,16 @@ import (
 	"github.com/spf13/cobra"
 )
 
-var execNthreads int
+var (
+	execNthreads int
+	storSystem   string
+	useNetappCLI bool
+	projectRoots map[string]string = map[string]string{
+		"netapp":  "/project",
+		"freenas": "/project_freenas",
+		"cephfs":  "/project_cephfs",
+	}
+)
 
 // loadNetAppCLI initialize interface to the NetAppCLI.
 func loadNetAppCLI() filergateway.NetAppCLI {
@@ -28,29 +38,44 @@ func loadNetAppCLI() filergateway.NetAppCLI {
 
 func init() {
 
-	projectActionExecCmd.Flags().IntVarP(&execNthreads, "nthreads", "n", 4, "`number` of concurrent worker threads.")
+	// get supported storage systems from the `projectRoots`.
+	supportedStorSystems := make([]string, len(projectRoots))
+	for sys := range projectRoots {
+		supportedStorSystems = append(supportedStorSystems, sys)
+	}
 
+	projectActionExecCmd.Flags().IntVarP(&execNthreads, "nthreads", "n", 4,
+		"`number` of concurrent worker threads.")
 	projectActionCmd.AddCommand(projectActionListCmd, projectActionExecCmd)
 
+	projectCmd.PersistentFlags().StringVarP(&storSystem, "sys", "s", "netapp",
+		fmt.Sprintf("storage `system`.  Supported systems: %s", strings.Join(supportedStorSystems, ",")))
+
+	projectCmd.PersistentFlags().BoolVarP(&useNetappCLI, "netapp-cli", "", false,
+		"use NetApp ONTAP CLI to apply changes on the NetApp filer. Only applicable for the netapp storage system.")
+
 	projectCmd.AddCommand(projectActionCmd, projectCreateCmd)
+
 	rootCmd.AddCommand(projectCmd)
 }
 
 var projectCmd = &cobra.Command{
 	Use:   "project",
-	Short: "Utility for project",
+	Short: "Utility for managing project storage",
 	Long:  ``,
+	PersistentPreRun: func(cmd *cobra.Command, args []string) {
+		if _, ok := projectRoots[storSystem]; !ok {
+			log.Fatalf("unsupported storage system: %s", storSystem)
+		}
+	},
 }
 
 var projectCreateCmd = &cobra.Command{
 	Use:   "create [projectID] [quotaGB]",
-	Short: "Utility for creating project with given quota",
+	Short: "Creates or updates project storage with given quota",
 	Long:  ``,
 	Args:  cobra.ExactArgs(2),
 	RunE: func(cmd *cobra.Command, args []string) error {
-		conf := loadConfig()
-		cli := filergateway.NetAppCLI{Config: conf.NetAppCLI}
-
 		iquota, err := strconv.Atoi(args[1])
 		if err != nil {
 			return err
@@ -58,25 +83,25 @@ var projectCreateCmd = &cobra.Command{
 
 		data := pdb.DataProjectUpdate{
 			Storage: pdb.Storage{
-				System:  "netapp",
+				System:  storSystem,
 				QuotaGb: iquota,
 			},
 		}
 
-		return cli.CreateProjectQtree(args[0], &data)
+		return actionExec(args[0], &data)
 	},
 }
 
 var projectActionCmd = &cobra.Command{
 	Use:   "action",
-	Short: "Utility for pending project actions",
+	Short: "Utility for managing pending project storage actions",
 	Long:  ``,
 }
 
 // subcommand to list pending project actions.
 var projectActionListCmd = &cobra.Command{
 	Use:   "list",
-	Short: "List pending project actions",
+	Short: "Lists pending project storage actions",
 	Long:  ``,
 	Args:  cobra.NoArgs,
 	RunE: func(cmd *cobra.Command, args []string) error {
@@ -106,7 +131,7 @@ var projectActionListCmd = &cobra.Command{
 // subcommand to execute pending project actions.
 var projectActionExecCmd = &cobra.Command{
 	Use:   "exec",
-	Short: "Execute pending project actions",
+	Short: "Executes pending project storage actions",
 	Args:  cobra.NoArgs,
 	RunE: func(cmd *cobra.Command, args []string) error {
 
@@ -152,69 +177,19 @@ func actionExec(pid string, act *pdb.DataProjectUpdate) error {
 	ipdb := loadPdb()
 	conf := loadConfig()
 
-	// METHOD1: use filer-gateway client to perform pending actions.
-	// fgw, err := filergateway.NewClient(conf)
-	// if err != nil {
-	// 	return err
-	// }
-	//
-	// perform pending actions via the filer gateway; write out
-	// error if failed and continue for the next project.
-	// if _, err := fgw.SyncUpdateProject(pid, act, time.Second); err != nil {
-	// 	return fmt.Errorf("failure updating project %s: %s", pid, err)
-	// }
-
-	// METHOD2: use netappcli interface to perform pending actions.
-	cli := filergateway.NetAppCLI{Config: conf.NetAppCLI}
-
-	actionsOK := make(map[string]*pdb.DataProjectUpdate)
-
 	log.Debugf("[%s] pending actions: %+v", pid, act)
 
+	// check if the action concerns creation of a new project.
+	// TODO: the project directory prefix should depends on storSystem
 	ppath := filepath.Join("/project", pid)
-
 	_, err := os.Stat(ppath)
 	newProject := !os.IsNotExist(err)
 
-	if !newProject {
-		log.Infof("[%s] project path already exists: %s", pid, ppath)
-		// // try update the project quota
-		// if err := cli.UpdateProjectQuota(pid, act); err != nil {
-		// 	return fmt.Errorf("[%s] fail updating project quota: %s", pid, err)
-		// }
-	} else {
-		log.Infof("[%s] creating new project on path: %s", pid, ppath)
-		if err := cli.CreateProjectQtree(pid, act); err != nil {
-			return fmt.Errorf("[%s] fail creating project: %s", pid, err)
-		}
-
-		t1 := time.Now()
-		// check until the project directory aappears
-		for {
-			if _, err := os.Stat(ppath); !os.IsNotExist(err) {
-				break
-			}
-			// timeout after 3 minutes
-			if time.Since(t1) > 3*time.Minute {
-				log.Errorf("[%s] timeout waiting for %s to appear", pid, ppath)
-				break
-			}
-			// wait for 100 millisecond for the next check.
-			time.Sleep(100 * time.Millisecond)
-		}
-
-		// move to next project if the ppath still doesn't exist.
-		if _, err := os.Stat(ppath); os.IsNotExist(err) {
-			return fmt.Errorf("[%s] project path not found: %s", pid, ppath)
-		}
-	}
-
-	// apply ACL setting based on the given project member roles.
+	// extract member roles from the `act`
 	managers := []string{}
 	contributors := []string{}
 	viewers := []string{}
 	removal := []string{}
-
 	for _, m := range act.Members {
 		switch m.Role {
 		case acl.Manager.String():
@@ -228,31 +203,48 @@ func actionExec(pid string, act *pdb.DataProjectUpdate) error {
 		}
 	}
 
-	// set members to the project
-	runner := acl.Runner{
-		RootPath:     ppath,
-		Managers:     strings.Join(managers, ","),
-		Contributors: strings.Join(contributors, ","),
-		Viewers:      strings.Join(viewers, ","),
-		FollowLink:   false,
-		SkipFiles:    false,
-		Nthreads:     4,
-		Silence:      false,
-		Traverse:     false,
-		Force:        false,
-	}
+	if useNetappCLI && storSystem == "netapp" {
+		// use NetappCLI + SSH to perform pending actions.
+		cli := filergateway.NetAppCLI{Config: conf.NetAppCLI}
+		if !newProject {
+			log.Infof("[%s] project path already exists: %s", pid, ppath)
+			// try update the project quota
+			if err := cli.UpdateProjectQuota(pid, act); err != nil {
+				return fmt.Errorf("[%s] fail updating project quota: %s", pid, err)
+			}
+		} else {
+			log.Infof("[%s] creating new project on path: %s", pid, ppath)
+			if err := cli.CreateProjectQtree(pid, act); err != nil {
+				return fmt.Errorf("[%s] fail creating project: %s", pid, err)
+			}
 
-	if ec, err := runner.SetRoles(); err != nil {
-		return fmt.Errorf("[%s] fail setting member role (ec=%d): %s", pid, ec, err)
-	}
+			t1 := time.Now()
+			// check until the project directory aappears
+			for {
+				if _, err := os.Stat(ppath); !os.IsNotExist(err) {
+					break
+				}
+				// timeout after 3 minutes
+				if time.Since(t1) > 3*time.Minute {
+					log.Errorf("[%s] timeout waiting for %s to appear", pid, ppath)
+					break
+				}
+				// wait for 100 millisecond for the next check.
+				time.Sleep(100 * time.Millisecond)
+			}
 
-	// remove members from project (only meaningful for existing project)
-	if !newProject {
-		runner = acl.Runner{
+			// move to next project if the ppath still doesn't exist.
+			if _, err := os.Stat(ppath); os.IsNotExist(err) {
+				return fmt.Errorf("[%s] project path not found: %s", pid, ppath)
+			}
+		}
+
+		// set members to the project
+		runner := acl.Runner{
 			RootPath:     ppath,
-			Managers:     strings.Join(removal, ","),
-			Contributors: strings.Join(removal, ","),
-			Viewers:      strings.Join(removal, ","),
+			Managers:     strings.Join(managers, ","),
+			Contributors: strings.Join(contributors, ","),
+			Viewers:      strings.Join(viewers, ","),
 			FollowLink:   false,
 			SkipFiles:    false,
 			Nthreads:     4,
@@ -261,18 +253,65 @@ func actionExec(pid string, act *pdb.DataProjectUpdate) error {
 			Force:        false,
 		}
 
-		if ec, err := runner.RemoveRoles(); err != nil {
-			return fmt.Errorf("[%s] fail removing acl (ec=%d): %s", pid, ec, err)
+		if ec, err := runner.SetRoles(); err != nil {
+			return fmt.Errorf("[%s] fail setting member role (ec=%d): %s", pid, ec, err)
+		}
+
+		// remove members from project (only meaningful for existing project)
+		if !newProject {
+			runner = acl.Runner{
+				RootPath:     ppath,
+				Managers:     strings.Join(removal, ","),
+				Contributors: strings.Join(removal, ","),
+				Viewers:      strings.Join(removal, ","),
+				FollowLink:   false,
+				SkipFiles:    false,
+				Nthreads:     4,
+				Silence:      false,
+				Traverse:     false,
+				Force:        false,
+			}
+
+			if ec, err := runner.RemoveRoles(); err != nil {
+				return fmt.Errorf("[%s] fail removing acl (ec=%d): %s", pid, ec, err)
+			}
+		}
+
+	} else {
+		// use filer-gateway client to perform pending actions.
+		fgw, err := filergateway.NewClient(conf)
+		if err != nil {
+			return err
+		}
+
+		// perform pending actions via the filer gateway; write out
+		// error if failed and continue for the next project.
+		if _, err := fgw.SyncUpdateProject(pid, act, time.Second); err != nil {
+			return fmt.Errorf("failure updating project %s: %s", pid, err)
+		}
+	}
+
+	// make sure the directory of the created project is owned by `project:project_g`
+	if newProject {
+		projectOwner := "project"
+		u, err := user.Lookup(projectOwner)
+		if err != nil {
+			log.Errorf("cannot find system user %s: %s", projectOwner, err)
+		}
+		uid, _ := strconv.Atoi(u.Uid)
+		gid, _ := strconv.Atoi(u.Gid)
+		if err := os.Chown(ppath, uid, gid); err != nil {
+			log.Errorf("cannot change owner of %s: %s", ppath, err)
 		}
 	}
 
 	// For PDBv1, get ACL from the project and update the active members into the database.
 	if v1, ok := ipdb.(pdb.V1); ok {
 
-		// let the runner to follow link, this allows to get acl from project directories
-		// hosted outside the /project folder. In this case, the returnning `acl.RolePathMap`
-		// from the runner will use the symlink target as path.
-		runner.FollowLink = true
+		runner := acl.Runner{
+			RootPath:   ppath,
+			FollowLink: false,
+		}
 
 		// get roles associated with the project directory, do not iterate over files/sub-directories.
 		rolePathMap, err := runner.GetRoles(false)
@@ -301,7 +340,10 @@ func actionExec(pid string, act *pdb.DataProjectUpdate) error {
 	}
 
 	// put successfully performed action to actionsOK map
-	actionsOK[pid] = act
+	// initialize map for successfully performed actions.
+	actionsOK := map[string]*pdb.DataProjectUpdate{
+		pid: act,
+	}
 
 	// clean up PDB pending actions that has been successfully performed.
 	// Note that it doesn't fail the process.
