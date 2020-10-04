0d5c8b126400d9f95fdaae77ef6bb3c575a2df2f
Hurng-Chun Lee <h.lee@donders.ru.nl>
2020-10-04 12:31:27 +0200

    added function to sent project ooq alert

--- pkg/mailer/init.go
+++ pkg/mailer/init.go
@@ -22,6 +22,54 @@ type Mailer struct {
 	config config.SMTPConfiguration
 }
 
+// AlertProjectStorageOoq sends out alert email concerning project (about to) running out-of-quota.
+func (m *Mailer) AlertProjectStorageOoq(recipient pdb.User, storageInfo pdb.StorageInfo, pid string) error {
+
+	from := "no-reply@donders.ru.nl"
+	name := fmt.Sprintf("%s %s", recipient.Firstname, recipient.Lastname)
+
+	uratio := 100 * storageInfo.UsageGb / storageInfo.QuotaGb
+	subject := fmt.Sprintf("Warning, storage of your project %s is %d%% full", pid, uratio)
+
+	// message template
+	tempStr := `Dear {{.Name}},
+
+You received this warning because you are the applicant and/or a manager and/or a contributor of the project {{.ProjectID}}.
+
+The quota for your project directory {{.ProjectID}} is with {{.QuotaUsageRatio}}% usage close to being full. 
+
+Be aware that when there is no quota any more, you may encounter issues such as:
+	
+    - not automatically receiving MEG and MRI raw data (see https://intranet.donders.ru.nl/index.php?id=archiving-autotransfer)
+	- not being able to use the lab uploader (see https://intranet.donders.ru.nl/index.php?id=uploader)
+	- unexpected failures in data analyses and batch jobs on the cluster
+	- etc.
+	
+Please consider to clean up the project directory (i.e. /project/{{.ProjectID}} or P:\{{.ProjectID}}) when possible.
+	
+If more quota is needed, please see the procedure described in the "Exceptional quota requests" section of the following intranet page: https://intranet.donders.ru.nl/index.php?id=quota
+	
+If you have further questions, don’t hesitate to contact the TG helpdesk (helpdesk@fcdonders.ru.nl).
+	
+Best regards, the DCCN Technical Group
+`
+
+	// data for message template
+	tempData := struct {
+		Name            string
+		ProjectID       string
+		QuotaUsageRatio int
+	}{name, pid, uratio}
+
+	body, err := composeMessageTempstr(tempStr, tempData)
+
+	if err != nil {
+		return err
+	}
+
+	return sendMail(m.config, from, recipient.Email, subject, body)
+}
+
 // NotifyProjectProvisioned sends out email notification
 // to `manager` about the just provisioned project `pid`.
 func (m *Mailer) NotifyProjectProvisioned(manager pdb.User, pid string) error {
