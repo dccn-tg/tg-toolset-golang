abab3183cd8ab73f385f9f903926b356ac09df0d
Hurng-Chun Lee <h.lee@donders.ru.nl>
2022-01-21 16:33:23 +0100

    print a warning on interruption

--- repository/internal/cmd/repocli/signal.go
+++ repository/internal/cmd/repocli/signal.go
@@ -21,7 +21,7 @@ func trapCancel(ctx context.Context) {
 
 	select {
 	case s := <-c:
-		log.Debugf("Got cancel signal %d, cancelling ...\n", s)
+		log.Warnf("Got cancel signal, cancelling ...\n", s)
 
 	case <-ctx.Done():
 	}
